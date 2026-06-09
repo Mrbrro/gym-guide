8544e67d6aa6fb6d899109878eda2927b4799857
Mrbrro <sasarogacov@gmail.com>
2026-06-09 08:38:50 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "nex-agi/nex-n2-pro:free",
+          model: "nvidia/nemotron-3.5-content-safety:free",
           messages,
           temperature: 0.5,
           max_tokens: 500
