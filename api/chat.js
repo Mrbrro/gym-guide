e3c404fc3ed65e958c6864034b65b85555254e09
Mrbrro <sasarogacov@gmail.com>
2026-06-09 08:34:07 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "google/gemma-4-31b:free",
+          model: "nex-agi/nex-n2-pro:free",
           messages,
           temperature: 0.5,
           max_tokens: 500
